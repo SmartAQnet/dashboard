0e4e44d9c1c1468d3782c590ceddae0e7df439e7
tremperteco <tremper@teco.edu>
2019-03-26 18:53:50 +0100

    add statistics

--- content/js/controllers/home-controller.js
+++ content/js/controllers/home-controller.js
@@ -1,4 +1,13 @@
 gostApp.controller('HomeCtrl', function ($scope, $http) {
-    $scope.Page.setTitle('GOST');
+    $scope.Page.setTitle('smartAQnet');
     $scope.Page.setHeaderIcon('');
+    
+    $http.get(getUrl() + "/v1.0/Things?$filter=datastreams/Observations/phenomenontime%20gt%20now()%20sub%20duration%27P1d%27&$count=true").then(function (response) {
+        $scope.active_devices = response.data["@iot.count"];
+    });
+
+    $http.get(getUrl() + "/v1.0/Observations?$top=0&$count=true").then(function (response) {
+        $scope.n_observations = response.data["@iot.count"];
+    });
+
 });
