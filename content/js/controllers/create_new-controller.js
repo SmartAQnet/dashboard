fc3df960ddf13c65cd315179d3eb855648082274
tremperteco <tremper@teco.edu>
2019-12-02 18:40:44 +0100

    further worked on the create_new feature

--- content/js/controllers/create_new-controller.js
+++ content/js/controllers/create_new-controller.js
@@ -38,32 +38,80 @@ gostApp.controller('CreateNewCtrl', function ($scope, $http, $routeParams) {
         return res
     };
     
-
+    $scope.getUnitOfMeasurement = function(obspropid){
+        $http.get(getUrl() + "/v1.0/ObservedProperties('" + obspropid + "')/properties/conventions").then(function (response) {
+            if(response.data.conventions.unitOfMeasurement) {
+                console.log(response.data.conventions.unitOfMeasurement);
+                $scope.newDatastream.unitOfMeasurement=response.data.conventions.unitOfMeasurement
+            };
+        }).catch(function (e){
+            console.log("Observerd Property does not have unit of measurement convention")
+        });
+    };
 
 
     //Things
 
 
+    //Sensors of the Thing
     $scope.getSensorsofThing = function(thingid){
         $http.get(getUrl() + "/v1.0/Things('" + thingid + "')/Datastreams?$expand=sensor&$select=sensor").then(function (response) {
-            $scope.thisThingsSensorsList = uniqueByID(Array.from(response.data.value.map(th => th.Sensor)));
+            $scope.thisThingsSensorList = uniqueByID(Array.from(response.data.value.map(th => th.Sensor)));
             $scope.originalSensorList = uniqueByID(Array.from(response.data.value.map(th => th.Sensor)));
         });
     };
 
     $scope.addSensortoThing = function(sensor){
-        $scope.thisThingsSensorsList.push(sensor);
-        $scope.newSensor = undefined;
+        if($scope.thisThingsSensorList.map(sens => sens["@iot.id"]).includes(sensor["@iot.id"])){
+            alert("This Sensor is already attached to the Thing") //implement this as toast
+        } else {
+            $scope.thisThingsSensorList.push(sensor);
+            $scope.newSensor = undefined;
+        }
+
     }; 
 
      $scope.removeSensorfromThing = function(sensor){
-            $scope.thisThingsSensorsList=remove($scope.thisThingsSensorsList,sensor)
+            $scope.thisThingsSensorList=remove($scope.thisThingsSensorList,sensor)
+    };
+
+    //Datastreams of the Thing
+    $scope.getDatastreamsofThing = function(thingid){
+        $http.get(getUrl() + "/v1.0/Things('" + thingid + "')/Datastreams").then(function (response) {
+            $scope.thisThingsDatastreamList = response.data.value;
+            $scope.originalDatastreamList = response.data.value;
+
+        });
+    };
+
+    $scope.addDatastreamtoThing = function(datastream){
+        if($scope.thisThingsDatastreamList.map(ds => ds["@iot.id"]).includes(datastream["@iot.id"])){
+            alert("This Datastream is already attached to the Thing") //implement this as toast
+        } else {
+            $scope.thisThingsDatastreamList.push(datastream);
+            $scope.newDatastream = undefined;
+        }
+
+    }; 
+
+     $scope.removeDatastreamfromThing = function(datastream){
+            $scope.thisThingsDatastreamList=remove($scope.thisThingsDatastreamList,datastream)
     };
 
 
-
-
-
+    //can try and catch for all components to give feedback on the error if necessary
+    $scope.generateDatastreamid = function(){
+        try{
+        unhashedid="saqn:ds:" + $scope.newThing.properties["operator.domain"] + ":" + $scope.newThing.properties["shortname"] + ":" + $scope.newThing.properties["operator.domain"] + ":"
+        + $scope.newDatastream.sensor.properties["manufacturer.domain"] + ":" + $scope.newDatastream.sensor.properties["shortname"] + ":"
+        + $scope.newDatastream.properties["hardware.serial_number"] + ":"
+        + $scope.newDatastream.observedproperty.properties["shortname"]
+        return({"unhashed":unhashedid,"@iot.id": sha1(unhashedid)})
+        } catch(e) {
+            return({"unhashed": undefined,"@iot.id": undefined}) 
+        }
+    };
+    
 
 
 
