683c3a3ad49f4da5ded205aad39957b4ab1882cb
tremperteco <tremper@teco.edu>
2019-12-20 17:07:41 +0100

    modified things controller and view to reflect frost query language

--- content/js/controllers/things-controller.js
+++ content/js/controllers/things-controller.js
@@ -1,22 +1,73 @@
-gostApp.controller('ThingsCtrl', function ($scope, $http, $routeParams) {
+gostApp.controller('ThingsCtrl', function ($scope, $http, $routeParams, $route) {
     $scope.Page.setTitle('THINGS');
     $scope.Page.setHeaderIcon(iconThing);
+
+    //defaults
     if(!("$orderby" in $routeParams)) $routeParams["$orderby"]="name asc";
 
+    if(!("$top" in $routeParams)){
+        $scope.newTop = 50;
+        $route.updateParams({'$top':$scope.newTop});
+    } else {$scope.newTop = $routeParams["$top"]}
+
+    
     //Implement Server Query Language in Static urls
     var query=getUrl() + "/v1.0/Things"+ Object.keys($routeParams).reduce(
 	    (a, i) => a + i + "=" + $routeParams[i] + "&","?").slice(0,-1) 
 
     $http.get(query).then(function (response) {
+
+        if($routeParams['$top']){
+            $scope.newTop = $routeParams['$top']
+        };
+
+        if($routeParams['$skip']){
+            $scope.thisLinkSkip = $routeParams['$skip']
+        };
+
         $scope.thingsList = response.data.value;
+        if(response.data["@iot.nextLink"]){
+            $scope.nextLinkSkip = (response.data['@iot.nextLink']).match(/(?:skip=)([0-9]+)/)[1]
+        } else {
+            $scope.nextLinkSkip = 0
+        };
     });
 
-    //Without Query Language
-    /*
-    $http.get(getUrl() + "/v1.0/Sensors").then(function (response) {
-        $scope.sensorsList = response.data.value;
-    });
+    /* //this has the problem that the query language allows more complex nesting with ";" between $parameters and deeper nesting with e.g. ()...;$expand=...($select=...))
+    //the code here takes care of the form "test(buu,baaa,boo),shubada(shwami,shaa),..."
+    $scope.keyIsExpandSelected = function(key, def=true){
+        if ($routeParams['$expand']){
+            $scope.expanddict = {}
+            $routeParams['$expand'].split("),").map(x => x.replace(")","")).map(y => $scope.expanddict[y.split("(")[0]] = y.split("(")[1].split(","))
+    }
     */
+
+    $scope.keyIsSelected = function(key, def=true){
+        if ($routeParams['$select']){
+            if($routeParams['$select'].split(',').includes(key)){ 
+                return true //if key present, return true
+            } else {
+                return false //if key not present, return false
+            }
+        } else {
+            return def //if no select parameter is given, return the given default, if no default given, return true (~no select is equal to show all)
+        }
+    };
+
+    $scope.followNextLink = function(){
+        $route.updateParams({'$skip':$scope.nextLinkSkip});
+    };
+
+    $scope.followPreviousLink = function(){
+        let skip = $scope.thisLinkSkip - $scope.newTop
+        if(skip < 0){skip = 0}
+        $route.updateParams({'$skip': skip})
+    };
+
+    $scope.setNewTop = function(){
+        $route.updateParams({'$top':$scope.newTop})
+    };
+
     
     $scope.thingClicked = function (thingID) {
         angular.forEach($scope.things, function (value, key) {
