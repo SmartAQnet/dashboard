5d2e41f70ce2f4c13766e2c135ab6c34a5ea0acd
tremperteco <tremper@teco.edu>
2020-07-02 18:39:50 +0200

    FoI Graphs now working; no datacompression or overview or anything yet

--- content/js/controllers/thing-controller.js
+++ content/js/controllers/thing-controller.js
@@ -11,48 +11,51 @@ gostApp.controller('ThingCtrl', function ($scope, $http, $routeParams, $location
     //pagination example for ng-repeat tables
     //http://jsfiddle.net/2ZzZB/56/
 
-    //also add and remove columns by defining all of them, then display none for those not listed in the route params. does not work for those that dont need expanding
-    // better: add all of them and let user enable with checkboxes and if necessary then add the route params?
-
-    //tabs of the entity (properties, location, ...) should then follow the scheme by taking the same query and add like things(saqn:t:...)/historicallocations
-    // --> thing/saqn:t:.../historicallocations ?
-    // also, why distinguish things and thing here? frost server doesnt. treat as new page things/saqn:t:... 
-
     //remove query params
     $location.search({})
 
-    //$scope.patchThing = {}
-    $scope.patchLocation = {}
 
+    //Load Data
     $http.get(getUrl() + "/v1.0/Things(" + getId($scope.id) + ")?$expand=Locations").then(function (response) {
-        $scope.name = response.data["name"];
-        $scope.description = response.data["description"];
-        $scope.properties = response.data["properties"];
-        $scope.Page.selectedThing = response.data;
-        //$scope.location = response.data["Locations"][0]["location"]["coordinates"];
-        $scope.showMap = true;
+  
+      $scope.name = response.data["name"];
+      $scope.description = response.data["description"];
+      $scope.properties = response.data["properties"];
+      $scope.Page.selectedThing = response.data;
+      $scope.showMap = true;
 
+      $scope.locationsList = response.data["Locations"];
+
+      if($scope.locationsList.length!= 0){
+      //display current thing location in another color --> change color of the respective pin
+      //not working atm, cant find the functions which are defined in the map controller... maybe add this function to the map controller where the view is centered and check the current scope
+      //highlightCurrentFeature($scope.locationsList[0]["location"]["coordinates"])         
+      //} else {
+        //alert("no location defined, please use 'move to new location' to define one")
+      }
+    });
+
+
+    //Load Thing Data for Patch Tree without any expands!
+    $http.get(getUrl() + "/v1.0/Things(" + getId($scope.id) + ")").then(function (response) {
+
+      //for patching Tree
       $scope.thing = response.data;
 
-        /*
-        $scope.patchThing.name = $scope.name;
-        $scope.patchThing.description = $scope.description;
-        $scope.patchThing.properties = $scope.properties;
-        $scope.patchThing.id = $scope.id;
-        */
-        $scope.patchtarget = $scope.Page.selectedThing["@iot.selfLink"]
-        $scope.patchpw = ''
-        $scope.pwvalid = ''
+      $scope.patchtarget = $scope.Page.selectedThing["@iot.selfLink"]
+      $scope.patchpw = ''
+      $scope.pwvalid = ''
 
       //on page load: if the thing does not have a properties field, set it with key unknown
       if(!$scope.thing.hasOwnProperty('properties')){
         $scope.thing["properties"] = {}
-      }
+      };
 
       //Initiate Tree: load entity to patch into format that is readable for angularjs
       $scope.item = {}
       $scope.item.items = []
       $scope.traverse($scope.thing,$scope.item.items)
+
       //console.log($scope.item.items)
       // check gives true that traverse and esrevart are indeed inverse to each other
       //console.log(JSON.stringify($scope.testthing)==JSON.stringify($scope.jsonobj))
@@ -62,39 +65,15 @@ gostApp.controller('ThingCtrl', function ($scope, $http, $routeParams, $location
         items: $scope.items
       }*/
 
-
     });
+
+
 
     // ----------------------------------------------------------------------------------------------------------------------
     // Traversing Nested Arrays with AngularJS, adapted from https://stackoverflow.com/questions/23315679/angularjs-traversing-nested-arrays 
 
 
     
-    /*unused
-    $scope.addSiblingItem = function(items, position) {
-      items.splice(position + 1, 0, {
-        key: 'sibling - new key',
-        value: 'sibling - new value',
-        items: []
-      });
-    }
-
-    $scope.addParentSibling = function(item) {
-      var parentSibling = {
-        key: 'aunt - key',
-        value: 'aunt - value',
-        items: []      
-      };
-      
-      if (item.items)
-      item.items.push(parentSibling);
-      else //root
-      item.push(parentSibling);    
-    }
-    */
-
-
-
     
     $scope.deleteMe = function(items, position) {
       items.splice(position, 1);
@@ -154,9 +133,11 @@ gostApp.controller('ThingCtrl', function ($scope, $http, $routeParams, $location
       return((obj.key != "hardware.id") && (obj.key != "shortname") && (obj.key != "operator.domain"))
     }
 
+    /*
     $scope.excludeOthers = function(obj){
-      return(obj.key != "Locations")
+      return((obj.key != "Locations") && (obj.key != "Datastreams"))
     }
+    */
 
     $scope.excludeAllProperties = function(obj){
       return(obj.key != "properties")
@@ -174,22 +155,14 @@ gostApp.controller('ThingCtrl', function ($scope, $http, $routeParams, $location
     $scope.tabLocationsClicked = function () {
     };
 
+    //load Location Data for Patching (separate, direct request without expands or anything for safety reasons when patching stuff)
+    $scope.patchLocation = {}
 
     $http.get(getUrl() + "/v1.0/Things(" + getId($scope.id) + ")/Locations").then(function (response) {
-        
-      $scope.locationsList = response.data.value;
-
-      if($scope.locationsList.length!= 0){
-      $scope.patchLocation = $scope.locationsList[0]
-
-      //display current thing location in another color --> change color of the respective pin
-      //not working atm, cant find the functions which are defined in the map controller... maybe add this function to the map controller where the view is centered and check the current scope
-      //highlightCurrentFeature($scope.locationsList[0]["location"]["coordinates"])         
-      //} else {
-        //alert("no location defined, please use 'move to new location' to define one")
-      }
-
-    });
+      if(response.data.value.length!=0){
+        $scope.patchLocation = response.data.value[0]
+      } //else here to maybe grey out patch button, force create new, whatever... 
+    })
 
     $scope.newLocation = {
       "name": "",
@@ -310,12 +283,43 @@ gostApp.controller('ThingCtrl', function ($scope, $http, $routeParams, $location
 
 
 
-  $scope.displayLocationTrace = false;
-  $scope.displayFoI = false;
+  $scope.locationtracedisplay = {"Latitude": true, "Longitude": true, "Altitude": true, "HistoricalLocations": true}
+  var locationData = {"HistoricalLocations": {"Longitude": {}, "Latitude": {}, "Altitude": {}}}
+  $scope.requestIsLoading={}
 
-  var datalon
-  var datalat
-  var dataalt
+  //basic intiation stuff
+  $http.get(getUrl() + "/v1.0/Things(" + getId($scope.id) + ")/Datastreams").then(function(response){
+
+    $scope.datastreamsList = response.data.value;
+    $scope.datastreamsList.forEach(function(ds){
+      
+      var dsid = ds["@iot.id"]
+
+      locationData[dsid]={}
+
+      $scope.requestIsLoading[dsid] = false
+
+      $scope.locationtracedisplay[dsid] = false
+      
+      let names = ["Longitude","Latitude","Altitude"]
+
+      //initiate empty FoI Datasets
+      names.forEach(function(name){
+        locationData[dsid][name]={
+          label: dsid + " " + name, 
+          coordinateType: name,
+          sourceType: dsid,
+          show: false,
+          steppedLine: "after", 
+          data: [],
+          borderColor: "rgba(" + Math.floor(Math.random()*255) + "," + Math.floor(Math.random()*255) + "," + Math.floor(Math.random()*255) + ",1)",
+          fill: false, 
+        }
+      })
+    });
+  });
+
+
 
   $scope.loadLocationTrace = function(){
 
@@ -325,34 +329,41 @@ gostApp.controller('ThingCtrl', function ($scope, $http, $routeParams, $location
       var res = response.data.value;
 
 
-      var resLabels = res.map(el => el["time"]);
-
-      datalon={
-        label: "Longitude", 
+      locationData["HistoricalLocations"]["Longitude"]={
+        label: "Historical Locations Longitude", 
+        coordinateType: "Longitude",
+        sourceType: "HistoricalLocations",
+        show: true, 
         steppedLine: "after", 
-        data: res.map(el => el.Locations[0]["location"]["coordinates"][0]),
+        data: res.map(function(el){
+          return ({x: el["time"], y: el.Locations[0]["location"]["coordinates"][0]})
+        }),
         borderColor: "rgba(156,115,135,1)",
         fill: false, 
       }
 
-      datalat={
-        label: "Latitude", 
+      locationData["HistoricalLocations"]["Latitude"]={
+        label: "Historical Locations Latitude", 
+        coordinateType: "Latitude",
+        sourceType: "HistoricalLocations",
+        show: true, 
         steppedLine: "after", 
-        data: res.map(el => el.Locations[0]["location"]["coordinates"][1]),
+        data: res.map(function(el){
+          return ({x: el["time"], y: el.Locations[0]["location"]["coordinates"][1]})
+        }),
         borderColor: "rgba(115,135,156,1)",
         fill: false, 
       }
 
-      dataalt = {
-        label: "Altitude", 
+      locationData["HistoricalLocations"]["Altitude"] = {
+        label: "Historical Locations Altitude", 
+        coordinateType: "Altitude",
+        sourceType: "HistoricalLocations",
+        show: true, 
         steppedLine: "after", 
-        data: res.map(function(el){
-          if(el.Locations[0]["location"]["coordinates"][2]){
-            return el.Locations[0]["location"]["coordinates"][2]
-          } else {
-            return undefined
-          }
-        }), 
+        data: res.filter(el => el.Locations[0]["location"]["coordinates"][2]).map(function(el){
+            return {x: el["time"], y: el.Locations[0]["location"]["coordinates"][2]}
+          }),
         borderColor: "rgba(135,156,115,1)",
         fill: false, 
       }
@@ -361,8 +372,8 @@ gostApp.controller('ThingCtrl', function ($scope, $http, $routeParams, $location
       locationChart = new Chart(ctx, {
           type: 'line',
           data: {
-              labels: resLabels,
-              datasets:[datalat,datalon,dataalt]
+              //labels: resLabels,
+              datasets:[locationData["HistoricalLocations"]["Longitude"],locationData["HistoricalLocations"]["Latitude"],locationData["HistoricalLocations"]["Altitude"]]
           }, 
           options: {
             tooltips: {
@@ -377,7 +388,7 @@ gostApp.controller('ThingCtrl', function ($scope, $http, $routeParams, $location
               aspectRatio: 2,
               title: {
                   display: true,
-                  text: "Historical Locations",
+                  text: "Locations",
               },
               scales: {
                   xAxes: [{
@@ -409,139 +420,87 @@ gostApp.controller('ThingCtrl', function ($scope, $http, $routeParams, $location
       });
     });
   };
+
+
 
   $scope.loadLocationTrace()
 
-  $scope.addData = function(name) {
-    // locationChart.data.labels.push(label);
-    [datalon,datalat,dataalt].forEach(function(el){
-      if(el.label == name){
-        locationChart.data.datasets.push(el)
-      }
-    });
-    locationChart.update();
+  $scope.toggleCoordinates = function(coord){
+    $scope.locationtracedisplay[coord] = !$scope.locationtracedisplay[coord]
+    console.log($scope.locationtracedisplay)
+    Object.keys(locationData).forEach(function(source){
+      locationData[source][coord]["show"] = ($scope.locationtracedisplay[coord] && $scope.locationtracedisplay[source])
+    })
+
+    $scope.updateChart()
   };
 
-  $scope.removeData = function(name) {
-    // locationChart.data.labels.pop();
-    let filteredarray = locationChart.data.datasets.filter(function(el){return el.label!=name})
-    locationChart.data.datasets=filteredarray
-    // locationChart.data.datasets.forEach((dataset) => {
-    //   dataset.pop();
-    // });
-    locationChart.update();
+
+  $scope.toggleSource = function(source){
+    //is done automatically by checkboxes
+    //$scope.locationtracedisplay[source] = !$scope.locationtracedisplay[source]
+
+    Object.keys(locationData[source]).forEach(function(coord){
+      locationData[source][coord]["show"] = ($scope.locationtracedisplay[coord] && $scope.locationtracedisplay[source])
+    })
+
+    $scope.updateChart()
   }
 
-  $scope.toggleData = function(name){
-    if(locationChart.data.datasets.map(el => el.label).includes(name)){
-      $scope.removeData(name)
-    } else {
-      $scope.addData(name)
-    }
-  }
+    //function that checks all "show" flags in locationData and updates the chart
+    $scope.updateChart = function(){
+      locationChart.data.datasets = []
+      Object.keys(locationData).forEach(function(key){
+        Object.keys(locationData[key]).forEach(function(coordinateType){
+          if(locationData[key][coordinateType]["show"]){
+            locationChart.data.datasets.push(locationData[key][coordinateType])
+          }
+        })
+      })
+      locationChart.update();
+    };
 
 
 
-  $scope.FoItop=50
-  $scope.FoIskip=0
-
-  var foilon
-  var foilat
-  var foialt
 
 
+
+
+
+
+
+
+  $scope.FoItop=1000
+
+
+
+  
   $scope.loadFoITrace = function(dsid){
 
-    // get information and construct the graph
-    $http.get(getUrl() + "/v1.0/Things(" + getId($scope.id) + ")/Datastreams('" + dsid + "')/Observations?$expand=FeatureOfInterest($select=feature)&$select=resultTime,FeatureOfInterest&$top=" + $scope.FoItop + "&$skip=" + $scope.FoIskip).then(function(response){
+    $scope.requestIsLoading[dsid] = true
+    var skip = locationData[dsid]["Latitude"]["data"].length
 
-      var res = response.data.value.map(function(el){
-        return {"resultTime": el["resultTime"], "coordinates": el["FeatureOfInterest"]["feature"]["coordinates"]}
-      })
+    $.snackbar({content: "Requesting Data. This may take a bit.", timeout: 5000})
+    // get information and add to dataset
+    $http.get(getUrl() + "/v1.0/Things(" + getId($scope.id) + ")/Datastreams('" + dsid + "')/Observations?$expand=FeatureOfInterest($select=feature)&$select=resultTime,FeatureOfInterest&$top=" + $scope.FoItop + "&$skip=" + skip).then(function(response){
+      
+      let res=response.data.value
+
+      locationData[dsid]["Longitude"]["data"]=locationData[dsid]["Longitude"]["data"].concat(res.map(function(el){
+        return ({x: el["resultTime"], y: el["FeatureOfInterest"]["feature"]["coordinates"][0]})
+      }))
+
+      locationData[dsid]["Latitude"]["data"]=locationData[dsid]["Latitude"]["data"].concat(res.map(function(el){
+        return ({x: el["resultTime"], y: el["FeatureOfInterest"]["feature"]["coordinates"][1]})
+      }))
+      
+      locationData[dsid]["Altitude"]["data"]=locationData[dsid]["Altitude"]["data"].concat(res.filter(el => el["FeatureOfInterest"]["feature"]["coordinates"][2]).map(function(el){
+        return ({x: el["resultTime"], y: el["FeatureOfInterest"]["feature"]["coordinates"][2]})
+      }))
 
 
-      var resLabels = res.map(el => el["resultTime"]);
-
-      foilon={
-        label: "Longitude", 
-        steppedLine: "after", 
-        data: res.map(el => el["coordinates"][0]),
-        borderColor: "rgba(156,115,135,1)",
-        fill: false, 
-      }
-
-      foilat={
-        label: "Latitude", 
-        steppedLine: "after", 
-        data: res.map(el => el["coordinates"][1]),
-        borderColor: "rgba(115,135,156,1)",
-        fill: false, 
-      }
-
-      foialt = {
-        label: "Altitude", 
-        steppedLine: "after", 
-        data: res.map(function(el){
-          if(el["coordinates"][2]){
-            return el["coordinates"][2]
-          } else {
-            return undefined
-          }
-        }), 
-        borderColor: "rgba(135,156,115,1)",
-        fill: false, 
-      }
-
-      var ctx = document.getElementById('LocationGraph').getContext('2d');
-      locationChart = new Chart(ctx, {
-          type: 'line',
-          data: {
-              labels: resLabels,
-              datasets:[foilat,foilon,foialt]
-          }, 
-          options: {
-            tooltips: {
-              mode: 'index',
-              intersect: false
-              },
-              hover: {
-                  mode: 'index',
-                  intersect: false
-              },
-              responsive: true,
-              aspectRatio: 2,
-              title: {
-                  display: true,
-                  text: "Feature of Interest",
-              },
-              scales: {
-                  xAxes: [{
-                      type: 'time',
-                      time: {
-                        tooltipFormat: 'YYYY-MM-DD hh:mm:ss', 
-                        displayFormats: {
-                          'millisecond': 'YYYY-MM-DD hh:mm:ss',
-                          'second': 'YYYY-MM-DD hh:mm:ss',
-                          'minute': 'YYYY-MM-DD hh:mm:ss',
-                          'hour': 'YYYY-MM-DD hh:mm:ss',
-                          'day': 'YYYY-MM-DD hh:mm:ss',
-                          'week': 'YYYY-MM-DD hh:mm:ss',
-                          'month': 'YYYY-MM-DD hh:mm:ss',
-                          'quarter': 'YYYY-MM-DD hh:mm:ss',
-                          'year': 'YYYY-MM-DD hh:mm:ss'
-                      }
-                      }
-                  }],
-                  yAxes: [{
-                    display: true,
-                    scaleLabel: {
-                      display: true,
-                      labelString: 'Coordinate'
-                    }
-                  }]
-              }
-          }
-      });
+      $scope.updateChart()
+      $scope.requestIsLoading[dsid] = false
     });
   };
 
